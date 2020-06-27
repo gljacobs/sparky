4d683f96ed1cf230ba00fcd62d148c5ee2004c6f
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-06-27 13:32:11 -0700

    add nav bar and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+//app components import
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      Hello Sparky
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+function Navbar() {
+    return (
+        <nav id="navbar">
+            <div className="nav-wrapper white">
+                <div className="container">
+                    <a href="../" className="brand-logo left blue-grey-text text-darken-3">SPARKY</a>
+                    <ul className="center">
+                        <li><a className="blue-grey-text text-darken-3" href="/about">About</a></li>
+                        <li><a className="blue-grey-text text-darken-3" href="/favorites">Favorites</a></li>
+                        <li><a className="blue-grey-text text-darken-3" href="/memorium">Memorium</a></li>
+                    </ul>
+                    <i className="fas fa-cat right blue-grey-text text-darken-3"></i>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
