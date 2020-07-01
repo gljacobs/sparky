e3f18128888602089778eb7bd90a33efbab7b5f9
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2020-07-01 14:58:03 -0700

    Updates

---
+++ precache-manifest.9fb5c90136907987d6e4e594a4569b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f7d752cfd09cf1725f350b19ef532b",
+    "url": "/sparky/index.html"
+  },
+  {
+    "revision": "915de3de95af3ee2d61a",
+    "url": "/sparky/static/css/main.1f14365b.chunk.css"
+  },
+  {
+    "revision": "bc32ac19bfc2917194c0",
+    "url": "/sparky/static/js/2.a5bed6ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sparky/static/js/2.a5bed6ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "915de3de95af3ee2d61a",
+    "url": "/sparky/static/js/main.8b119dcd.chunk.js"
+  },
+  {
+    "revision": "d0e23e58b516f5a3a841",
+    "url": "/sparky/static/js/runtime-main.18c810b3.js"
+  }
+]);
